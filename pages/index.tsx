bda9c814825ff5c82c0af1776508fbd9f77eb8b8
mrjackmaster <jacektrg+github@gmail.com>
2022-07-16 00:31:24 +0200

    Changing width

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 
 const Home: NextPage = () => {
 	return (
-		<div className='col-6'>
+		<div className='col-9'>
 			<Head>
 				<title>Units Converter</title>
 				<meta name='description' content='Units Converter' />
