d39375894ce6fcebb35dd4fac7b540e60fb14ddb
mrjackmaster <jacektrg+github@gmail.com>
2022-07-16 00:35:02 +0200

    Adding state and handler for search input. Adding in/decrement buttons and handlers. Reworking data rendering into table element. Updating render function accordingly.

--- pages/currency/index.tsx
+++ pages/currency/index.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from 'next';
 import React, { useRef, useState } from 'react';
 import styles from '../../styles/Currency.module.scss';
+import cc from 'currency-codes';
 
 interface Props {
 	rates: { [key: string]: number };
@@ -8,8 +9,10 @@ interface Props {
 
 const Currency: NextPage<Props> = ({ rates }) => {
 	const [inputValue, setInputValue] = useState(0);
+	const [searchValue, setSearchValue] = useState('');
 
 	const inputRef = useRef<HTMLInputElement>(null);
+	const searchRef = useRef<HTMLInputElement>(null);
 
 	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const value = Number(e.target.value);
@@ -22,43 +25,119 @@ const Currency: NextPage<Props> = ({ rates }) => {
 		}
 	};
 
-	const handleClear = () => {
+	const handleIncrement = () => {
+		if (inputValue >= 999999) {
+			setInputValue(999999);
+		} else {
+			setInputValue((prevState) => prevState + 1);
+		}
+	};
+
+	const handleDecrement = () => {
+		if (inputValue <= 0) {
+			setInputValue(0);
+		} else {
+			setInputValue((prevState) => prevState - 1);
+		}
+	};
+
+	const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setSearchValue(e.target.value);
+	};
+
+	const handleInputClear = () => {
 		setInputValue(0);
 		inputRef.current?.select();
+	};
+
+	const handleSearchClear = () => {
+		setSearchValue('');
+		searchRef.current?.select();
 	};
 
 	const conversionsRender = () => {
 		const render = [];
 		for (const [key, value] of Object.entries(rates)) {
-			render.push(
-				<div className='col-3'>
-					{key}: {(inputValue * value).toFixed(2)}
-				</div>
-			);
+			const ccData = cc.code(key);
+			if (ccData) {
+				render.push(
+					<tr key={key}>
+						<td>{ccData.code}</td>
+						<td>{Math.round(inputValue * value * 100) / 100}</td>
+						<td className='d-none d-md-table-cell'>{ccData.currency}</td>
+						<td className='d-none d-lg-table-cell'>
+							{ccData.countries.join(', ').replace(' (The)', '')}
+						</td>
+					</tr>
+				);
+			}
 		}
 		return render;
 	};
 
 	return (
-		<div className='col-6'>
-			<div>
-				<div>
+		<div className='col-5'>
+			<div className={`${styles.controls} mb-3`}>
+				<div className='input-group'>
 					<label htmlFor='value'></label>
 					<input
+						className='form-control'
 						ref={inputRef}
 						type='number'
 						id='value'
 						min={0}
 						max={999999}
+						step={0.01}
 						value={inputValue}
 						onChange={handleInputChange}
 					/>
-					<button type='button' onClick={handleClear}>
+					<div>
+						<button
+							type='button'
+							className={styles.caret_up}
+							onClick={handleIncrement}
+						></button>
+						<button
+							type='button'
+							className={styles.caret_down}
+							onClick={handleDecrement}
+						></button>
+					</div>
+					<button type='button' onClick={handleInputClear}>
 						Clear
 					</button>
 				</div>
 			</div>
-			<div className='row'>{rates ? conversionsRender() : null}</div>
+			<div className='mb-3'>
+				<div className='input-group'>
+					<label htmlFor='value'></label>
+					<input
+						className='form-control'
+						ref={searchRef}
+						type='text'
+						id='value'
+						minLength={3}
+						maxLength={32}
+						placeholder='Search'
+						value={searchValue}
+						onChange={handleSearchChange}
+					/>
+					<button type='button' onClick={handleSearchClear}>
+						Clear
+					</button>
+				</div>
+			</div>
+			<table className={`${styles.table} table table-striped`}>
+				<thead className='table-primary'>
+					<tr>
+						<th scope='col-1'></th>
+						<th scope='col-2'></th>
+						<th scope='col-3' className='d-none d-md-table-cell'></th>
+						<th scope='col-2' className='d-none d-lg-table-cell'></th>
+					</tr>
+				</thead>
+				<tbody>{rates ? conversionsRender() : null}</tbody>
+			</table>
 		</div>
 	);
 };
