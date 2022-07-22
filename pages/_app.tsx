30e463a8fad872972795acc5a2ec0ced5f025527
Jacek Gizinski <jacektrg+github@gmail.com>
2022-07-22 17:15:15 +0200

    Adding bootstrap bg color and padding class. Renaming class

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import type { AppProps } from 'next/app';
 import Link from 'next/link';
 import Navbar from '../components/Navbar';
-import styles from '../styles/Home.module.scss';
 import Footer from '../components/Footer';
+import styles from '../styles/Main.module.scss';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
 	return (
 		<div className={styles.container}>
-			<div className={styles.title}>
+			<div className={`${styles.title} bg-dark p-2`}>
 				<Link href='/'>Units Converter</Link>
 			</div>
-			<main className={`${styles.main} row`}>
+			<main className={`${styles.content} row`}>
 				<div className='col-6'>
 					<Component {...pageProps} />
 				</div>
