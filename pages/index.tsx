a9c30a6e6a75be910fa7dac580d73cd090494d14
Jacek Gizinski <jacektrg+github@gmail.com>
2022-07-22 17:15:57 +0200

    Adding list of available conversion to home page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../styles/Main.module.scss';
 
 const Home: NextPage = () => {
 	return (
-		<div className='col-9'>
+		<div className={`${styles.home} col-9`}>
 			<Head>
 				<title>Units Converter</title>
 				<meta name='description' content='Units Converter' />
@@ -11,7 +13,18 @@ const Home: NextPage = () => {
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-			<div>{'Use navigation ->'}</div>
+			<h4>Available conversions:</h4>
+			<ul className='list-group my-3'>
+				<li className='list-group-item list-group-item-action list-group-item-action list-group-item-warning'>
+					<Link href='/currency'>Currency conversion</Link>
+				</li>
+				<li className='list-group-item list-group-item-action list-group-item-action list-group-item-success'>
+					<Link href='/length'>Length conversion</Link>
+				</li>
+				<li className='list-group-item list-group-item-action list-group-item-action list-group-item-danger'>
+					<Link href='/temperature'>Temperature conversion</Link>
+				</li>
+			</ul>
 		</div>
 	);
 };
