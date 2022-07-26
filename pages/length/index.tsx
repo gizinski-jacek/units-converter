78cd26077cf53ff27ff6376634b21148a331819d
Jacek Gizinski <jacektrg+github@gmail.com>
2022-07-26 17:46:13 +0200

    Refactoring.
    Moving length input, its controls and handlers to separate component.

--- pages/length/index.tsx
+++ pages/length/index.tsx
@@ -1,51 +1,20 @@
 import { NextPage } from 'next';
-import { useRef, useState } from 'react';
-import { Table } from 'react-bootstrap';
-import Caret from '../../reusables/Caret';
-import ResetBtn from '../../reusables/ResetBtn';
+import { useState } from 'react';
+import { Form, InputGroup, Table } from 'react-bootstrap';
 import lengthData from '../../data/lengthData';
+import FormInput from '../../reusables/FormInput';
 
 const Length: NextPage = () => {
 	const [inputValue, setInputValue] = useState(0);
 	const [chosenLength, setChosenLength] = useState('meter');
 
-	const inputRef = useRef<HTMLInputElement>(null);
-
-	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const value = Number(e.target.value);
-		if (value < 0) {
-			setInputValue(0);
-		} else if (value > 999999) {
-			setInputValue(999999);
-		} else {
-			setInputValue(value);
-		}
+	const handleInputChange = (value: number) => {
+		setInputValue(value);
 	};
 
 	const handleLengthChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
 		const value = e.target.value;
 		setChosenLength(value);
-	};
-
-	const handleIncrement = () => {
-		if (inputValue >= 999999) {
-			setInputValue(999999);
-		} else {
-			setInputValue((prevState) => Math.round((prevState + 0.01) * 100) / 100);
-		}
-	};
-
-	const handleDecrement = () => {
-		if (inputValue <= 0) {
-			setInputValue(0);
-		} else {
-			setInputValue((prevState) => Math.round((prevState - 0.01) * 100) / 100);
-		}
-	};
-
-	const handleInputClear = () => {
-		setInputValue(0);
-		inputRef.current?.select();
 	};
 
 	const lengthListRender = lengthData.map((item) => {
@@ -72,33 +41,17 @@ const Length: NextPage = () => {
 	return (
 		<div>
 			<div className='mb-3'>
-				<div className='input-group'>
-					<select
-						className='form-select'
+				<InputGroup>
+					<Form.Select
 						name='currency'
 						id='currency'
 						value={chosenLength}
 						onChange={(e) => handleLengthChange(e)}
 					>
 						{lengthListRender}
-					</select>
-					<input
-						className='form-control'
-						ref={inputRef}
-						type='number'
-						id='value'
-						min={0}
-						max={999999}
-						step={0.01}
-						value={Math.round(inputValue * 100) / 100}
-						onChange={handleInputChange}
-					/>
-					<div className='ms-1'>
-						<Caret upwards={true} cta={handleIncrement} />
-						<Caret upwards={false} cta={handleDecrement} />
-					</div>
-					<ResetBtn cta={handleInputClear} />
-				</div>
+					</Form.Select>
+					<FormInput updateParent={handleInputChange} />
+				</InputGroup>
 			</div>
 			<Table striped bordered hover>
 				<thead className='table-dark'>
